3da086a98a5efe655326be002d2eaeccfb5250c3
Peter Chen <plc003@ucsd.edu>
2018-10-31 21:27:57 -0700

    added sidebar html, css, js

--- js/tabs.js
+++ js/tabs.js
@@ -11,3 +11,13 @@ function eventTabs(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
