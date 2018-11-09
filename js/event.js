0553bdae3f216b843671c061624a65b2edb1b8cf
Aditi Kilambi <kilambi.aditi@gmail.com>
2018-11-08 19:12:07 -0800

    Added see more function and prepared for submission

---
+++ js/event.js
@@ -0,0 +1,11 @@
+function clickIt(i){
+  document.getElementById('seemore' + i).className = "dontShow";
+  document.getElementById('seeless' + i).className = "displayIt";
+  document.getElementById('longBoi' + i).className = "displayIt";
+}
+
+function clickItBack(i){
+  document.getElementById('seemore' + i).className = "displayIt";
+  document.getElementById('seeless' + i).className = "dontShow";
+  document.getElementById('longBoi' + i).className = "dontShow";
+}
--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-		// Initialize Firebase
+// Initialize Firebase
 var config = {
   apiKey: "AIzaSyDHQWb-9NmoPuKRzAwt9Tja7FNEyYECQnU",
   authDomain: "eventfinder-1e474.firebaseapp.com",
@@ -45,50 +45,184 @@ $(window).load(function() {
 	$('#addEventForm').submit(addEvent);
 });
 
+
+var searchterms = function() {
+	var organization = $( "#Organization option:selected" ).val();
+	localStorage.setItem('search', organization);
+}
+
 $(window).load(function() {
-	allEvents.once('value',function(snapshot)
+	$('#searchbar').submit(searchterms);
+});
+
+/*  Method for populating myEvents Page with only events from the organization */
+$(window).load(function() {
+	allEvents.once('value',function(snapshot) 
 	{
-		var x = ' ';
-		snapshot.forEach(function(snapshot) {
+		var x = ' ';	
+		var i = 0;
+		snapshot.forEach(function(snapshot) {	
 			var obj = snapshot.val();
-			x = x +
-        	"<div class='container event'>" +
-          		"<div class='row'>" +
-		            "<div class='col-sm-3 date'>" +
-		       		 	"<p>" + obj.date + "</p>" +
-		       		 "</div>" +
+			if(obj.organization === "Latin Club") {
+					x = x + 
+		        	"<div class='container event'>" +
+		          		"<div class='row'>" +
+				            "<div class='col-sm-3 date'>" +
+				       		 	"<p>" + obj.date + "</p>" +
+				       		 "</div>" +
 
-		       		 "<div class='col-sm-8 title'>"+
-		       		 	"<p>" + obj.name + "</p>" +
-		       		 "</div>" +
+				       		 "<div class='col-sm-8 title'>"+
+				       		 	"<p>" + obj.name + "</p>" +
+				       		 "</div>" +
 
-					"<div class='col-sm-2 share'>" +
-		         	 	"<a href='./createEvent.html'><button>Edit Event</button></a>" +
-		         	 	"<a href='./share.html'><button>" + "<i class='fa fa-share-alt' style='font-size:24px'></i></button></a>" +
-		       		 "</div>" +
-       		 	"</div>"+
+							"<div 'col-sm-2 share'>" +
+				         	 	"<a href='./createEvent.html'><button>Edit Event</button></a>" +
+				         	 	"<a href='./share.html'><button>" + "<i class='fa fa-share-alt' style='font-size:24px'></i></button></a>" +
+				       		 "</div>" +
+		       		 	"</div>"+
 
-	       		 "<div class='col-sm-3' id='eventimage'>" +
-	          		"<img class='img-responsive' src='./images/500holder.png'/>" +
-	        	 "</div>" +
+			       		 "<div class='col-sm-3' id='eventimage'>" +
+			          		"<img class='img-responsive' src='./images/500holder.png'/>" +
+			        	 "</div>" +
 
-	        	 "<div class='col-sm-9 description'>"+
+			        	 "<div class='col-sm-9 description'>"+
 
-	        	 	"<p> <strong>Time:</strong> " + obj.startTime + " - " + obj.endTime +
-	        	 	"<p> <strong>Organization:</strong> " + obj.organization + "</p>" +
-	        	 	"<p> <strong>Location:</strong> " + obj.location + "</p>" +
-	        	 	"<p> <strong>Description: </strong>" + obj.description + "</p>" +
+			        	 	"<p> <strong>Time:</strong> " + obj.startTime + " - " + obj.endTime +
+			        	 	"<p> <strong>Organization:</strong> " + obj.organization + "</p>" +
+			        	 	"<p> <strong>Location:</strong> " + obj.location + "</p>" +
+			        	 	"<p> <strong>Description: </strong>" + obj.description + "</p>" +
+			        	 	"<p id='longBoi" + i + "' class='dontshow'> <strong> Details: </strong>" + obj.longDes + "</p>" +
 
 
-	              "<div class='seemore'>" +
-	            	"<a href='./seemore.html'>see more</a>" +
-	              "</div>" +
-	              "</div>" +
-
-              "</div><br><br><br>";
-
-		 	if (document.getElementById('pgContent') == null) return;
-		 	document.getElementById('pgContent').innerHTML = x
+			              "<div id='seemore" + i + "'class='displayIt' onclick='clickIt(" + i + ")'>" +
+			            	"<p><button>see more</button></p>" +
+			              "</div>" +
+				       
+				          "<div id='seeless" + i + "'class='dontshow' onclick='clickItBack("+ i + ")'>" +
+				          	"<p><button>see less</button></p>" +
+            			  "</div>" +
+			              "</div>" +
+		              "</div> <br><br><br>";
+	            }
+		 	if (document.getElementById('myEventContent') == null) return;
+		 	document.getElementById('myEventContent').innerHTML = x;
+		 	i = i + 1;
 		});
 	});
+});
+
+
+$(window).load(function() {
+	if(localStorage.getItem('search') == null) {
+		allEvents.once('value',function(snapshot) 
+		{
+			var x = ' ';	
+			var i = 0;
+			snapshot.forEach(function(snapshot) {	
+				var obj = snapshot.val();
+					x = x + 
+		        	"<div class='container event'>" +
+		          		"<div class='row'>" +
+				            "<div class='col-sm-3 date'>" +
+				       		 	"<p>" + obj.date + "</p>" +
+				       		 "</div>" +
+
+				       		 "<div class='col-sm-8 title'>"+
+				       		 	"<p>" + obj.name + "</p>" +
+				       		 "</div>" +
+
+							"<div 'col-sm-2 share'>" +
+				         	 	"<a href='./createEvent.html'><button>Edit Event</button></a>" +
+				         	 	"<a href='./share.html'><button>" + "<i class='fa fa-share-alt' style='font-size:24px'></i></button></a>" +
+				       		 "</div>" +
+		       		 	"</div>"+
+
+			       		 "<div class='col-sm-3' id='eventimage'>" +
+			          		"<img class='img-responsive' src='./images/500holder.png'/>" +
+			        	 "</div>" +
+
+			        	 "<div class='col-sm-9 description'>"+
+
+			        	 	"<p> <strong>Time:</strong> " + obj.startTime + " - " + obj.endTime +
+			        	 	"<p> <strong>Organization:</strong> " + obj.organization + "</p>" +
+			        	 	"<p> <strong>Location:</strong> " + obj.location + "</p>" +
+			        	 	"<p> <strong>Description: </strong>" + obj.description + "</p>" +
+			        	 	"<p id='longBoi" + i + "' class='dontshow'> <strong> Details: </strong>" + obj.longDes + "</p>" +
+
+
+			              "<div id='seemore" + i + "'class='displayIt' onclick='clickIt(" + i + ")'>" +
+			            	"<p><button>see more</button></p>" +
+			              "</div>" +
+				       
+				          "<div id='seeless" + i + "'class='dontshow' onclick='clickItBack("+ i + ")'>" +
+				          	"<p><button>see less</button></p>" +
+            			  "</div>" +
+			              "</div>" +
+		              "</div> <br><br><br>";
+			 	if (document.getElementById('allEventContent') == null) return;
+			 	document.getElementById('allEventContent').innerHTML = x;
+			 	i = i + 1;
+			});
+		});
+	}
+});
+
+$(window).load(function() {
+	if(localStorage.getItem('search') != null) {
+
+		var toSearch = localStorage.getItem('search');
+
+		allEvents.orderByChild('organization').equalTo(toSearch).once('value',function(snapshot) 
+		{
+			var x = ' ';
+			var i = 0;	
+			snapshot.forEach(function(snapshot) {	
+				var obj = snapshot.val();
+					x = x + 
+		        	"<div class='container event'>" +
+		          		"<div class='row'>" +
+				            "<div class='col-sm-3 date'>" +
+				       		 	"<p>" + obj.date + "</p>" +
+				       		 "</div>" +
+
+				       		 "<div class='col-sm-8 title'>"+
+				       		 	"<p>" + obj.name + "</p>" +
+				       		 "</div>" +
+
+							"<div 'col-sm-2 share'>" +
+				         	 	"<a href='./createEvent.html'><button>Edit Event</button></a>" +
+				         	 	"<a href='./share.html'><button>" + "<i class='fa fa-share-alt' style='font-size:24px'></i></button></a>" +
+				       		 "</div>" +
+		       		 	"</div>"+
+
+			       		 "<div class='col-sm-3' id='eventimage'>" +
+			          		"<img class='img-responsive' src='./images/500holder.png'/>" +
+			        	 "</div>" +
+
+			        	 "<div class='col-sm-9 description'>"+
+
+			        	 	"<p> <strong>Time:</strong> " + obj.startTime + " - " + obj.endTime +
+			        	 	"<p> <strong>Organization:</strong> " + obj.organization + "</p>" +
+			        	 	"<p> <strong>Location:</strong> " + obj.location + "</p>" +
+			        	 	"<p> <strong>Description: </strong>" + obj.description + "</p>" +
+			        	 	"<p id='longBoi" + i + "' class='dontshow'> <strong> Details: </strong>" + obj.longDes + "</p>" +
+
+
+			              "<div id='seemore" + i + "'class='displayIt' onclick='clickIt(" + i + ")'>" +
+			            	"<p><button>see more</button></p>" +
+			              "</div>" +
+				       
+				          "<div id='seeless" + i + "'class='dontshow' onclick='clickItBack("+ i + ")'>" +
+				          	"<p><button>see less</button></p>" +
+            			  "</div>" +
+			              "</div>" +
+		              "</div> <br><br><br>";
+
+			 	if (document.getElementById('allEventContent') == null) return;
+			 	document.getElementById('allEventContent').innerHTML = x;
+			    i = i + 1;
+			});
+		});
+		localStorage.removeItem('search');
+	}
 });
